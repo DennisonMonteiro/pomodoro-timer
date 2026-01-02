9d34f6812f8cc9c9e150033398a19f19d622dd6c
Dennison Monteiro <dennison.monteiro@iaudit.com.br>
2026-01-02 19:41:03 -0300

    feat: add routes do project

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,20 @@
-import { Text, View } from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import { Text, TouchableOpacity, View } from "react-native"
 
 export const HomePage = () => {
+    const navigation = useNavigation<any>();
+
     return(
         <View>
             <Text style={{ fontFamily: 'InterRegular', fontSize: 40 }}>
                 Home Page!
             </Text>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+                <Text>
+                    Settings
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
